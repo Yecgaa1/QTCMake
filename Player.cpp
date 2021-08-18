6dc779e996ac43ccce24c71a10c186c08eb93f62
xtx <2656823393@qq.com>
2021-08-18 18:37:11 +0800

    123

---
+++ Player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by xtx on 2021/8/17.
+//
+
+#include "player.h"
---
+++ Player.h
@@ -0,0 +1,16 @@
+//
+// Created by xtx on 2021/8/17.
+//
+
+#ifndef QTCMAKE_PLAYER_H
+#define QTCMAKE_PLAYER_H
+
+
+
+class player {
+
+};
+
+
+
+#endif //QTCMAKE_PLAYER_H
